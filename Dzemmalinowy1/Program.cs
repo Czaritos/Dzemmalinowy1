71caedec51828e7a4b7fe29742a08925103d6f03
Cezary Kruczynski <CKruczynski19@outlook.com>
2023-11-23 17:30:25 +0100

    dzien 3 pd

--- Dzemmalinowy1/Program.cs
+++ Dzemmalinowy1/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var age = 21;
+var name = "Ewa";
+var gender = "Kobieta";
+
+if (age == 33 && name =="Ewa")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (gender == "Kobieta")
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta ponizej 30tki");
+    }
+    else
+    {
+        Console.WriteLine("Kobieta w wieku 30lat lub wiecej");
+    }
+}
+else
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mezczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Pełnoletni mezczyzna");
+    }
+}
